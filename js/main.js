1092726939377b1a765719f648fdae8e5cc4569e
jiangenhe <jiangenhe@gmail.com>
2023-03-28 15:19:58 -0400

    reusable component of scatterplot

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ d3.csv('data/cities_and_population_area.csv')
         d.population = parseInt(d.population)
       })
 
+      const scatterplot = new Scatterplot({parentElement:"body", containerHeight: 400}, data)
+      scatterplot.updateVis()
+
+      d3.select('body').append('button').text('add data').on('click', function () {
+        data.push({country: "United States", city: "Knoxville", population: 1861730000, area: 104.3})
+        scatterplot.data = data
+        scatterplot.updateVis()
+      })
+
+
     })
--- js/scatterplot.js
+++ js/scatterplot.js
@@ -7,7 +7,7 @@ class Scatterplot {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 600,
-      containerHeight: _config.containerHeight || 400,
+      containerHeight: _config.containerHeight || 500,
       margin: _config.margin || {top: 25, right: 20, bottom: 20, left: 35},
     }
     this.data = _data;
@@ -25,23 +25,32 @@ class Scatterplot {
     vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
     // Scales
-
+    vis.xScale = d3.scaleLinear()
+      .range([0, vis.width])
+    vis.yScale = d3.scaleLinear()
+      .range([0, vis.height])
 
     // Initialize axes
-
+    vis.xAxis = d3.axisBottom(vis.xScale)
+    vis.yAxis = d3.axisLeft(vis.yScale)
 
     // Define size of SVG drawing area
-
+    vis.svg = d3.select(vis.config.parentElement).append('svg')
+      .attr('width', this.config.containerWidth)
+      .attr('height', this.config.containerHeight)
 
     // Append group element that will contain our actual chart
     // and position it according to the given margin config
-
+    vis.visG = vis.svg.append('g')
+      .attr('transform', `translate(${this.config.margin.left},${this.config.margin.top})`)
 
     // Append empty x-axis group and move it to the bottom of the chart
-
+    vis.xAxisG = vis.svg.append('g')
+      .attr('transform', `translate(${this.config.margin.left},${this.config.margin.top + vis.height})`)
 
     // Append y-axis group
-
+    vis.yAxisG = vis.svg.append('g')
+      .attr('transform', `translate(${this.config.margin.left},${this.config.margin.top})`)
 
   }
 
@@ -55,7 +64,9 @@ class Scatterplot {
 
 
     // Set the scale input domains
-
+    vis.xScale.domain([0, d3.max(vis.data, d => d.population)])
+    vis.yScale.domain([0, d3.max(vis.data, d => d.area)])
+    this.renderVis()
   }
 
   /**
@@ -65,9 +76,15 @@ class Scatterplot {
     let vis = this;
 
     // Add circles
-
+    vis.visG.selectAll('circle')
+      .data(vis.data)
+      .join('circle')
+      .attr('cx', d => vis.xScale(d.population))
+      .attr('cy', d => vis.yScale(d.area))
+      .attr('r', 5)
 
     // Update the axes
-
+    vis.xAxisG.call(this.xAxis)
+    vis.yAxisG.call(this.yAxis)
   }
 }
